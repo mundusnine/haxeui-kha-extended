9a63278781a5c696106fd9f671e524ec3ab491c0
jsn213 <jean-sebastien.nadeau.3@ulaval.ca>
2020-02-10 23:39:28 -0500

    Add ways to define how intespector node field buttons react to click events. Make a way to intialize a specific component(i.e. if we have a rigidbody dont have the + sign on the button but have a - sign)

--- haxe/ui/extended/InspectorView.hx
+++ haxe/ui/extended/InspectorView.hx
@@ -3,7 +3,9 @@ package haxe.ui.extended;
 import haxe.ui.data.DataSource;
 import haxe.ui.containers.ScrollView;
 import haxe.ui.components.Label;
+import haxe.ui.components.Button;
 import haxe.ui.events.UIEvent;
+import haxe.ui.events.MouseEvent;
 import haxe.ui.core.Component;
 import haxe.ui.extended.InspectorNode;
 
@@ -12,6 +14,8 @@ class InspectorView extends TreeView {
 
     public var updateData:UIEvent->Void = null;
     public var curNode:InspectorNode = null;
+    public var initFields:Map<String,Component->Void> = new Map<String,Component->Void>();
+    public var buttonsClick:Map<String,MouseEvent->Void> = new Map<String,MouseEvent->Void>();
     override function get_dataSource(){
         return i_dataSource;
     }
@@ -33,6 +37,18 @@ class InspectorView extends TreeView {
         if(!filterOut(data.name)){
             curNode  = new InspectorNode(cast(data),this,updateData);
             feed.addComponent(curNode);
+            for(fname in initFields.keys()){
+                var comp = curNode.findComponent(fname);
+                if(comp != null){
+                    initFields.get(fname)(comp);
+                }
+            }
+            for(bname in buttonsClick.keys()){
+                var butt:Button = curNode.findComponent(bname,Button);
+                if(butt != null){
+                    butt.onClick = buttonsClick.get(bname);
+                }
+            }
         }
     }
 }
